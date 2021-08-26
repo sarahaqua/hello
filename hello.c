d1d54f0ade33b94a397d2bb663fd10869670839c
Sarah Lowderman <slowderman2@huskers.unl.edu>
2021-08-26 15:31:07 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sarah Lowderman
+ * Date: 2021/08/26
+ * 
+ * A C program that outputs my name.
+ */
+ 
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+
+     printf("Sarah Lowderman\n");
+
+     return 0;
+ }
